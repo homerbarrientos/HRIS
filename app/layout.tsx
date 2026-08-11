c564a51b50a56af6bbbb56b2b9919350335125b4
homerbarrientos <88486402+homerbarrientos@users.noreply.github.com>
2026-08-12 02:30:18 +0800

    Load employee directory styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./employee-directory.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
