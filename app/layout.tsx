4d370cba9b051348a7f31b51adaa2adc2632cb55
homerbarrientos <88486402+homerbarrientos@users.noreply.github.com>
2026-08-12 03:55:58 +0800

    Load leave module styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./employee-directory.css";
 import "./employee-profile.css";
+import "./leave.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
